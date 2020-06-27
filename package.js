f735d8134ad2a211af2048b685d2059254be8c25
Julius Tio <juliusardentio@gmail.com>
2020-06-27 23:31:28 +1000

    Bump version to 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'juliusarden:subdomain-login',
-  version: '0.0.2',
+  version: '1.0.0',
   summary: 'Persistent Login across subdomains using cookies',
   git: 'https://github.com/juliusarden/meteor-subdomain-login',
   documentation: 'README.md'
